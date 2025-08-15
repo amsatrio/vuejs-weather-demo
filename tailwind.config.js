0bbfedc975f765891e83c2126be20b85afe6c91c
Andre Mochammad Satrio <andre.satrio@ms.mii.co.id>
2025-08-15 09:27:44 +0700

    implement colors, font family, container, screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,ts,js,tsx,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71"
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"]
+    },
+    container: {
+      padding: "2rem",
+      center: true
+    },
+    screens: {
+      sm: "640px",
+      md: "768px"
+    }
   },
   plugins: [],
 }
