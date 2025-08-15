37ace059adc42d643efbdaed8e5860ea00a1db2e
amsatrio <79563756+amsatrio@users.noreply.github.com>
2025-08-15 11:18:00 +0700

    update tailwindcss to v4 and add default modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         <p>hello world!</p>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     </div>
 </template>
 
 <script setup>
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
