f2d87c6a88e8065bed33f389b9bca52c60406d28
amsatrio <79563756+amsatrio@users.noreply.github.com>
2025-08-15 11:21:40 +0700

    move breakpoint to tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,ts,js,tsx,jsx}'],
   theme: {
-    extend: {
-      colors: {
-        "weather-primary": "#00668A",
-        "weather-secondary": "#004E71"
-      }
-    },
-    fontFamily: {
-      Roboto: ["Roboto", "sans-serif"]
-    },
-    container: {
-      padding: "2rem",
-      center: true
-    },
-    screens: {
-      sm: "640px",
-      md: "768px"
-    }
+    extend: {}
   },
   plugins: [],
 }
